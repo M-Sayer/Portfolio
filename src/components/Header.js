4b1a1939434305f8e557204dfcf688c097b9f1f6
M-Sayer <haji.san@icloud.com>
2020-08-25 21:48:04 -0400

    git init

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Menu from './Menu'
+
+const Header = () => {
+
+  return (
+   <header>
+      <h1>muhajir</h1>
+      <Menu />
+   </header>
+  )
+}
+
+export default Header
---
+++ src/components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Menu = () => {
+
+  return (
+    <h1>menu</h1>
+  )
+}
+
+export default Menu
