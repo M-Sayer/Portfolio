5ab99d48c5e8d6ccfc29d01af715f7696d7168ef
M-Sayer <haji.san@icloud.com>
2020-08-26 04:37:55 -0400

    intro sections

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,13 @@ const Main = () => {
 
   return (
     <h1>main</h1>
+    <section className='intro'>
+      <h2>salutations!</h2>
+      <p>
+      i'm muhajir, a full stack software developer.
+      i build web applications using technologies such as React, Node.js, express.js and PostgreSQL, utilizing my experience from digital marketing and working with e-commerce brands to craft creative software solutions.
+      </p>
+    </section>
   )
 }
 
