f27df4acede67ecd5c2f3b712721091929b37a92
M-Sayer <haji.san@icloud.com>
2020-08-29 13:59:55 -0400

    rotating hello animation

---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { StyledHello } from './Hello.styled';
+
+const Hello = () => {
+  return (
+    <StyledHello>
+      <div>
+        hello
+      </div>
+      <div>
+        bonjour
+      </div>
+      <div>
+        hola
+      </div>
+    </StyledHello>
+  )
+}
+
+export default Hello
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './Main.css'
 
+import Hello from './Hello/Hello';
+
 const Main = () => {
 
   const [greeting, setGreeting] = useState('hello')
@@ -13,21 +15,23 @@ const Main = () => {
   }, 2000)
 
   return (
-    <div className='intro'>
-      <div className='hello-rotate'>
-        <span>hello</span>
-        <span>bonjour</span>
-        <span>hola</span>
+    <div className='main'>
+      <div className='greeting'>
+        <Hello />
+        <div className='typewriter-container'>
+          <div className='typewriter-name'>
+            i'm muhajir,
+          </div>
+          <div className='typewriter-text'>
+          a full stack software developer.
+          </div>
+        </div>
       </div>
-      <div className='typewriter-name'>
-        i'm muhajir,
-      </div>
-     <div className='typewriter-text'>
-      a full stack software developer.
+      <div className='bio-short'>
+       <p>
+       i build web applications, utilizing my experience from digital marketing and working with e-commerce brands to craft creative software solutions and user experiences that are easy to use.
+       </p>
      </div>
-      <p>
-      i build web applications, utilizing my experience from digital marketing and working with e-commerce brands to craft creative software solutions and user experiences that are easy to use.
-      </p>
     </div>
   );
 }
